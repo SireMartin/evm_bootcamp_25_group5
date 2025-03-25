f0d1b8a99626aba5cde38dac8f696fbafebb179c
Maarten Deheegher <made@skylux.be>
2025-03-25 16:28:21 +0100

    completed lesson 14

--- siremartin/lesson13/scaffold-eth-2/packages/nextjs/app/debug/page.tsx
+++ siremartin/lesson13/scaffold-eth-2/packages/nextjs/app/debug/page.tsx
@@ -1,5 +1,6 @@
 import { DebugContracts } from "./_components/DebugContracts";
 import type { NextPage } from "next";
+import { useReadContract } from "wagmi";
 import { getMetadata } from "~~/utils/scaffold-eth/getMetadata";
 
 export const metadata = getMetadata({
@@ -24,5 +25,82 @@ const Debug: NextPage = () => {
     </>
   );
 };
+
+function TokenInfo(params: { address: `0x${string}` }) {
+  return (
+    <div className="card w-96 bg-primary text-primary-content mt-4">
+      <div className="card-body">
+        <h2 className="card-title">Testing useReadContract wagmi hook</h2>
+        <TokenName></TokenName>
+        <TokenBalance address={params.address}></TokenBalance>
+        <TokenInfo address={params.address as `0x${string}`}></TokenInfo>
+      </div>
+    </div>
+  );
+}
+
+function TokenName() {
+  const { data, isError, isLoading } = useReadContract({
+    address: "0x37dBD10E7994AAcF6132cac7d33bcA899bd2C660",
+    abi: [
+      {
+        constant: true,
+        inputs: [],
+        name: "name",
+        outputs: [
+          {
+            name: "",
+            type: "string",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    functionName: "name",
+  });
+
+  const name = typeof data === "string" ? data : 0;
+
+  if (isLoading) return <div>Fetching name…</div>;
+  if (isError) return <div>Error fetching name</div>;
+  return <div>Token name: {name}</div>;
+}
+
+function TokenBalance(params: { address: `0x${string}` }) {
+  const { data, isError, isLoading } = useReadContract({
+    address: "0xff198373b61fc6132efe421a47637a17c75986c8",
+    abi: [
+      {
+        constant: true,
+        inputs: [
+          {
+            name: "_owner",
+            type: "address",
+          },
+        ],
+        name: "balanceOf",
+        outputs: [
+          {
+            name: "balance",
+            type: "uint256",
+          },
+        ],
+        payable: false,
+        stateMutability: "view",
+        type: "function",
+      },
+    ],
+    functionName: "balanceOf",
+    args: [params.address],
+  });
+
+  const balance = typeof data === "number" ? data : 0;
+
+  if (isLoading) return <div>Fetching balance…</div>;
+  if (isError) return <div>Error fetching balance</div>;
+  return <div>Balance: {balance}</div>;
+}
 
 export default Debug;
