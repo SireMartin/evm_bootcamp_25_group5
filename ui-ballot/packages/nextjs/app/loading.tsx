42480c3c7a4321982598ecb3fe7e7eba10b19306
D-CRYPTO-DEV <165848363+D-CRYPTO-DEV@users.noreply.github.com>
2025-04-16 21:26:09 -0700

    Delete ui-ballot directory

--- ui-ballot/packages/nextjs/app/loading.tsx
+++
@@ -1,8 +0,0 @@
-export default function Loading() {
-  return (
-    <div className="flex min-h-screen flex-col items-center justify-center">
-      <div className="loading loading-spinner loading-lg"></div>
-      <p className="mt-4 text-lg">Loading...</p>
-    </div>
-  );
-} 
