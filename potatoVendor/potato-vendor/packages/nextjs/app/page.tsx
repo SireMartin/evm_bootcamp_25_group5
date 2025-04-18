48912247c9dc126c883455f8a3ddfd5c0b22d192
Marco <binghy@users.noreply.github.com>
2025-04-18 16:09:53 +0200

    Update page.tsx

--- potatoVendor/potato-vendor/packages/nextjs/app/page.tsx
+++ potatoVendor/potato-vendor/packages/nextjs/app/page.tsx
@@ -17,13 +17,22 @@ const Home: NextPage = () => {
             <span className="block text-2xl mb-2">Welcome to</span>
             <span className="block text-4xl font-bold">Potato Vendor</span>
           </h1>
-          {connectedAddress && (
-            <div className="flex justify-center items-center space-x-2 flex-col">
-              <p className="my-2 font-medium">Connected Address:</p>
-              <Address address={connectedAddress} />
-            </div>
-          )}
+          <div className="flex justify-center my-4">
+            <Image
+            src="/images/super_potato.png"                        // Path to the image
+            alt="Super Potato"                                    // Alt text for accessibility
+            width={400}                                           // Adjust width as needed
+            height={400}                                          // Adjust height as needed
+            priority                                              // Optional: Ensure the image loads quickly
+          />
         </div>
+        {connectedAddress && (
+          <div className="flex justify-center items-center space-x-2 flex-col">
+            <p className="my-2 font-medium">Connected Address:</p>
+            <Address address={connectedAddress} />
+          </div>
+        )}
+      </div>
 
         <div className="flex-grow bg-base-300 w-full mt-16 px-8 py-12">
           <div className="flex justify-center items-center gap-12 flex-col md:flex-row">
